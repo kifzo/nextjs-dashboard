9b43d896509ed27b256036c76820d3604aa7012c
mba <enoshimawhitebait@gmail.com>
2025-03-12 14:46:16 +0900

    部分的な事前レンダリングの実験的な実装を追加し、Next.jsの設定を更新しました。

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+// 部分的な事前レンダリングの実装するため experimental_ppr セグメント構成オプションを追加。
+export const experimental_ppr = true;
 
 export default function RootLayout({
   children,
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Partial Prerendering(PPR)の実験的(experimental)な実装
+  // incremental(増分) 値を使用すると、特定のルートに PPR を採用できます。
+  experimental: {
+    ppr: 'incremental'
+  }
 };
 
 export default nextConfig;
