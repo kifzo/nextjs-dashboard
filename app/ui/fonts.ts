cdbf47b7bf5dd5263d2ea5c6062775ec247ced56
mba <enoshimawhitebait@gmail.com>
2025-03-12 15:11:24 +0900

    異なるフォントウェイトを指定するためにLusitanaフォントの設定を更新しました

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,6 +3,6 @@ import { Inter, Lusitana } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
 
 export const lusitana = Lusitana({
-  weight: ['400', '700'],
+  weight: ['400', '700'],  // 異なるフォントウェイト指定。 たとえば、(通常) と(太字)
   subsets: ['latin'],
 });
