5bec2499430276061be42c18fed239e0d908630f
mba <enoshimawhitebait@gmail.com>
2025-03-17 00:38:31 +0900

    メタデータを追加し、Invoicesページおよびルートレイアウトのタイトルと説明を設定しました。また、部分的な事前レンダリングのオプションを追加しました。

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,7 +6,11 @@ import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { fetchInvoicesPages } from "@/app/lib/data";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Invoices",
+};
 // searchParamsはNext.jsがURLのクエリパラメータを解析して自動的に渡している。特定の親コンポーネントからpropsを受け取っているわけではなく、Next.jsのルーティングシステムによって自動的にクエリパラメータが渡されている
 export default async function Page(props: {
   searchParams?: Promise<{
